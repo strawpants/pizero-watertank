d9ad091b53421024b78113344f7cfc76899a9e12
Roelof Rietbroek <roelof@wobbly.earth>
2023-10-12 09:02:01 +0100

    Convert range to wwater level in the tank

--- wetcave/dashboard/views.py
+++ wetcave/dashboard/views.py
@@ -8,19 +8,22 @@ from plotly.offline import plot
 import plotly.graph_objs as go
 from django_pandas.io import read_frame
 import numpy as np
-
+from datetime import datetime,timedelta
+from settings.models import TankConfig
 speedofsoundconstant=340 #m/s
 
-def waterlevelPlot():
-    qry=SensorData.objects.all()
+def waterlevelPlot(sounderHeight):
+    #get since last week
+    since=datetime.now()-timedelta(days=7)
+    qry=SensorData.objects.filter(epoch__gte=since)
     sensordf=read_frame(qry)
     sensordf['vsound']=20.05*(273.16 + sensordf['temperature']).apply(np.sqrt)
     sensordf['deltavsound']=speedofsoundconstant-20.05*(273.16 + sensordf['temperature']).apply(np.sqrt)
-    
+    sensordf['waterlevel']= sounderHeight*1e2-sensordf['traveltime']*sensordf['vsound']*1e-4/2
     fig=go.Figure()
 
     # fig.add_trace(go.Line(name = 'Range', x = sensordf['epoch'], y = sensordf['traveltime']*speedofsoundconstant*1e-4/2))
-    fig.add_trace(go.Line(name = 'Range (Temp corrected)', x = sensordf['epoch'], y = sensordf['traveltime']*sensordf['vsound']*1e-4/2))
+    fig.add_trace(go.Scatter(name = 'Water level (Temp corrected)', x = sensordf['epoch'], y =sensordf['waterlevel'],mode="lines+markers"))
 
     # fig.add_trace(go.Line(name = 'Delta Range (Temp effect)', x = sensordf['epoch'], y = sensordf['traveltime']*sensordf['deltavsound']*1e-4/2))
     # fig.add_trace(go.Line(name = 'Delta Range (Temp effect)', x = sensordf['epoch'], y = sensordf['pressure']))
@@ -28,8 +31,8 @@ def waterlevelPlot():
 
     #Update layout 
     fig.update_layout(title_text = 'Tank water level',
-                      xaxis_title = 'time',
-                      yaxis_title = 'Range [cm]')
+            xaxis_title = 'time',
+                      yaxis_title = 'height [cm]')
     
     #Turn graph object into local plotly graph
     plotly_plot_obj = plot({'data': fig}, output_type='div')
@@ -39,5 +42,7 @@ def waterlevelPlot():
 
 @login_required()
 def dashboard(request):
-    plotlyhtml=waterlevelPlot() 
+    # retrieve the settings
+    settings=TankConfig.objects.first()
+    plotlyhtml=waterlevelPlot(settings.sounderheight) 
     return render(request,"dashboard.html",context={"content":plotlyhtml})
